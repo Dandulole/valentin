f108afbd999843bea0182879814ee272a59bb9a4
Dandulole <revas@list.ru>
2017-04-10 21:32:39 +0300

    create and add views/site/index.php

--- www/protected/views/site/index.php
+++ www/protected/views/site/index.php
@@ -4,17 +4,108 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
-<ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
-</ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<div class="slogan-wr">
+		<div class="main"></div>
+	</div>
+	<div class="our-services">
+		<div class="main">
+			<h2>Наши услуги</h2>
+			<ul>
+				<li>
+					<img src="images/icon1.png" alt="">
+					<div>
+						<h3>Проектирование</h3>
+						<p>Разработка проектной документации на любые слаботочные системы. Мы выполняем проектную документацию разделов СС, ПС, ОС, СКД, СОТ, ЛВС</p>
+					</div>
+				</li>
+				<li>
+					<img src="images/icon2.png" alt="">
+					<div>
+						<h3>Монтаж СКС</h3>
+						<p>Разработка проектной документации на любые слаботочные системы. Мы выполняем проектную документацию разделов СС, ПС, ОС, СКД, СОТ, ЛВС</p>
+					</div>
+				</li>
+				<li>
+					<img src="images/icon3.png" alt="">
+					<div>
+						<h3>Проектирование</h3>
+						<p>Монтаж систем охранно-пожарной сигнализации, систем видеонаблюдения, систем контроля доступом, систем оповещения, систем пожарной автоматики, локально вычислительных сетей</p>
+					</div>
+				</li>
+				<li>
+					<img src="images/icon4.png" alt="">
+					<div>
+						<h3>Проектирование</h3>
+						<p>Монтаж систем охранно-пожарной сигнализации, систем видеонаблюдения, систем контроля доступом, систем оповещения, систем пожарной автоматики, локально вычислительных сетей</p>
+					</div>
+				</li>
+				<li>
+					<img src="images/icon5.png" alt="">
+					<div>
+						<h3>Проектирование</h3>
+						<p>Разработка проектной документации на любые слаботочные системы. Мы выполняем проектную документацию разделов СС, ПС, ОС, СКД, СОТ, ЛВС</p>
+					</div>
+				</li>
+				<li>
+					<img src="images/icon6.png" alt="">
+					<div>
+						<h3>Проектирование</h3>
+						<p>Разработка проектной документации на любые слаботочные системы. Мы выполняем проектную документацию разделов СС, ПС, ОС, СКД, СОТ, ЛВС</p>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="why-we">
+		<div class="main">
+			<ul>
+				<li>
+					<img src="images/why1.png" alt="">
+					<p>огромный<br/> опыт</p>
+				</li>
+				<li>
+					<img src="images/why2.png" alt="">
+					<p>Оптимальные<br/> решения</p>
+				</li>
+				<li>
+					<img src="images/why3.png" alt="">
+					<p>Качество</p>
+				</li>
+				<li>
+					<img src="images/why4.png" alt="">
+					<p>Гарантия</p>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="news-wr">
+		<div class="main">
+			<h2><span>Новости</span></h2>
+			<ul>
+				<li>
+					<p class="date">24.10.2016</p>
+					<p class="capt">Заголовок новости</p>
+					<p class="text-excerpt">
+						Монтаж систем охранно-пожарной сигнализации, систем видеонаблюдения, систем контроля доступом, систем оповещения, систем пожарной автоматики
+					</p>
+					<a href="#" class="more">подробнее >></a>
+				</li>
+				<li>
+					<p class="date">24.10.2016</p>
+					<p class="capt">Заголовок новости</p>
+					<p class="text-excerpt">
+						Монтаж систем охранно-пожарной сигнализации, систем видеонаблюдения, систем контроля доступом, систем оповещения, систем пожарной автоматики
+					</p>
+					<a href="#" class="more">подробнее >></a>
+				</li>
+				<li>
+					<p class="date">24.10.2016</p>
+					<p class="capt">Заголовок новости</p>
+					<p class="text-excerpt">
+						Монтаж систем охранно-пожарной сигнализации, систем видеонаблюдения, систем контроля доступом, систем оповещения, систем пожарной автоматики
+					</p>
+					<a href="#" class="more">подробнее >></a>
+				</li>
+			</ul>
+		</div>
+	</div>
